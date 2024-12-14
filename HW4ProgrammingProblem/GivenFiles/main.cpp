e589b25dcda81ad5c74aebe3884579312d05e6ba
Jackson Clary <141957445+t0ll1v3r@users.noreply.github.com>
2024-12-14 10:42:02 -0500

    Update and rename HW4ProgrammingProblem/main.cpp to HW4ProgrammingProblem/GivenFiles/main.cpp

--- HW4ProgrammingProblem/GivenFiles/main.cpp
+++ HW4ProgrammingProblem/GivenFiles/main.cpp
@@ -1,9 +1,9 @@
-#include "Problem5.h"
+#include "FindMissingNumber.h"
 #include <iostream>
 
 int main(int argc, char* argv[]) {
     InputArray inp = InputArray();
-    int result = Problem5(inp);
+    int result = FindMissingNumber(inp);
     cout << "The missing number from the example array is: " << result << ".\n";
     return result;
 }
