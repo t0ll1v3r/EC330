38f3b4663b474474014c0b1cc806fbe2fdb332da
Jackson Clary <141957445+t0ll1v3r@users.noreply.github.com>
2024-12-14 10:39:07 -0500

    Update FindMissingNumber.cpp

--- HW4ProgrammingProblem/FindMissingNumber.cpp
+++ HW4ProgrammingProblem/FindMissingNumber.cpp
@@ -7,36 +7,36 @@ The missing number is reconstructed by checking discrepancies bit by bit. Overal
 #include "InputArray.h"
 #include <iostream>
 
-int Problem5(InputArray& arr) {
+int FindMissingNumber(InputArray& arr) {
     int n = arr.n;
     int missing = 0;
     int numBits = 0;
     
-    //determine number of bits required to represent n
+    // determine number of bits required to represent n
     while ((1 << numBits) <= n) {
         numBits++;
     }
 
-    //process each bits position
+    // process each bits position
     for (int bit = 0; bit < numBits; bit++) {
         int expectedCount = 0;
         int actualCount = 0;
 
-        //expected count of 1's at the current bit position if no number is missing
+        // expected count of 1's at the current bit position if no number is missing
         for (int i = 0; i <= n; i++) {
             if (i & (1 << bit)) {
                 expectedCount++;
             }
         }
 
-        //actual count of 1's at the current bit position in the array
+        // actual count of 1's at the current bit position in the array
         for (int i = 0; i < n; i++) {
             if (arr.findBit(i, bit)) {
                 actualCount++;
             }
         }
 
-        //if the counts don't match, the missing number has a 1 in this bit position
+        // if the counts don't match, the missing number has a 1 in this bit position
         if (actualCount != expectedCount) {
             missing |= (1 << bit);
         }
